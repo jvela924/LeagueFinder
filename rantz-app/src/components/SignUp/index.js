9e2cb21fbcf0e3b0b52c95162f8ea26cf1fb8d2a
Jeremy Vela <jvela924@gmail.com>
2019-11-07 13:49:48 -0500

    added signup form

--- rantz-app/src/components/SignUp/index.js
+++ rantz-app/src/components/SignUp/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom'
 
 import * as ROUTES from '../../constants/routes'
@@ -10,19 +10,72 @@ const SignUpPage = () => (
   </div>
 );
 
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+}
+
 class SignUpForm extends Component {
   constructor(props) {
     super(props)
+    this.state = { ...INITIAL_STATE }
   }
   onSubmit = event => {
 
   }
   onChange = event => {
-
+    this.setState({ [event.target.name]: event.target.value })
   }
   render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
     return (
       <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">Sign Up</button>
+        {error && <p>{error.message}</p>}
       </form>
     )
   }
@@ -34,6 +87,6 @@ const SignUpLink = () => (
   </p>
 )
 
-export default SignUp;
+export default SignUpPage;
 
 export { SignUpForm, SignUpLink };
