547aac4999a29f09605404fe3c66033015ac2eb3
Jeremy Vela <jvela924@gmail.com>
2019-11-07 10:48:58 -0500

    added the auth interface for Firebase

--- rantz-app/src/components/Firebase/firebase.js
+++ rantz-app/src/components/Firebase/firebase.js
@@ -23,6 +23,13 @@ class Firebase {
 
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
