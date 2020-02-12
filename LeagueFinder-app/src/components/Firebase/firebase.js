477a93fb4c4ab0654e1fb448724dc5b946399502
Jeremy Vela <jvela924@gmail.com>
2020-02-11 23:50:36 -0500

    changed name

---
+++ LeagueFinder-app/src/components/Firebase/firebase.js
@@ -0,0 +1,41 @@
+import app from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: "AIzaSyDS8Y8cYC-znev4BaEBtlryZx6VOHllDnE",
+  authDomain: "rantz-f2723.firebaseapp.com",
+  databaseURL: "https://rantz-f2723.firebaseio.com",
+  projectId: "rantz-f2723",
+  storageBucket: "rantz-f2723.appspot.com",
+  messagingSenderId: "706659373311",
+  appId: "1:706659373311:web:0b6b34d0059c3036003cca",
+  measurementId: "G-EBC9QTQBTT"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+
+    this.auth = app.auth();
+    this.db = app.database();
+  }
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+
+    user = uid => this.db.ref(`users/${uid}`);
+
+    users = () => this.db.ref('users')
+}
+
+export default Firebase;
